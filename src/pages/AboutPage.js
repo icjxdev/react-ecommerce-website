1d6c7a9f2bdd419eeee893ca7f70f2f0f2286b4d
Isaac Chew <isaaccjx00@gmail.com>
2023-03-04 18:24:34 +0800

    added hero section on home page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -14,10 +14,16 @@ const AboutPage = () => {
             <h2>our story</h2>
             <div className="underline"></div>
             <p>
-              lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-              atque ex excepturi aut sapiente possimus tempore ratione dolorum
-              id nam rem, qui, mollitia, similique quia maiores unde porro eum
-              saepe?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+              accusantium sapiente tempora sed dolore esse deserunt eaque
+              excepturi, delectus error accusamus vel eligendi, omnis beatae.
+              Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+              dolore, obcaecati incidunt sequi blanditiis est exercitationem
+              molestiae delectus saepe odio eligendi modi porro eaque in libero
+              minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+              ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+              similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+              iste.
             </p>
           </div>
         </article>
