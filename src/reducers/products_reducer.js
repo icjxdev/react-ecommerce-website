91719e5897ffa7f99bc4c4f0654e7f35fabeee3d
Isaac Chew <isaaccjx00@gmail.com>
2023-03-06 16:37:58 +0800

    fetching single product

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -29,6 +29,24 @@ const products_reducer = (state, action) => {
       };
     case GET_PRODUCTS_ERROR:
       return { ...state, products_loading: false, products_error: true };
+    case GET_SINGLE_PRODUCT_BEGIN:
+      return {
+        ...state,
+        single_product_loading: true,
+        single_product_error: false,
+      };
+    case GET_SINGLE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product: action.payload,
+      };
+    case GET_SINGLE_PRODUCT_ERROR:
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product_error: true,
+      };
     default:
       throw new Error(`No matching "${action.type}" - action type`);
   }
