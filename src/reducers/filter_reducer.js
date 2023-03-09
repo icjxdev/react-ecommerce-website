84e7443c095ea26b28a6aa683b37787bf48db47c
Isaac Chew <isaaccjx00@gmail.com>
2023-03-09 16:20:19 +0800

    setting up controlled input in sort component

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,6 +17,24 @@ const filter_reducer = (state, action) => {
       all_products: [...action.payload],
     };
   }
+  if (action.type === SET_GRIDVIEW) {
+    return {
+      ...state,
+      grid_view: true,
+    };
+  }
+  if (action.type === SET_LISTVIEW) {
+    return {
+      ...state,
+      grid_view: false,
+    };
+  }
+  if (action.type === UPDATE_SORT) {
+    return {
+      ...state,
+      sort: action.payload,
+    };
+  }
 
   throw new Error(`No Matching "${action.type}" - action type`);
 };
